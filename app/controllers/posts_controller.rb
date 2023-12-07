b9ffab97495e009dfd3621e0d5cc6dac0236e286
yutaiwahashi <yuta.iwahahsi@gmail.com>
2023-12-07 21:54:02 +0900

    first commit
    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index  # 追記する
+  end
+ 
+ end
